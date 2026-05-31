8f51961b50418b9d481c6178650b352c4414976d
casey <caseyriley94@gmail.com>
2026-05-31 00:56:35 -0400

    comment out 'nosys deSystemize' functionality; possibly to be re-integrated later, but for now to be compatible with flake-parts more rapidly

--- paisano/grow/newImportSignatureFor.nix
+++ paisano/grow/newImportSignatureFor.nix
@@ -1,6 +1,11 @@
 /*
 This file implements the unique import signature of each block.
 */
+#FIXME:
+# Options
+#1. Remove entirely, "file seems to be taking on 'input modifications' for nosys"; may need to pull out/remap some attrs
+#2. Remove deSystemize, and check functionality of returns
+#3. Find out What 'exactly' deSystemize returns, and replace with flake-parts equivalent
 {
   l,
   deSystemize,
@@ -27,10 +32,15 @@ in
       then cfg.inputs.nixpkgs
       else null;
   in (
-    (deSystemize system (cfg.inputs // additionalInputs))
+    (cfg.inputs // additionalInputs)
+    #FIXME: Dropped the below deSystemize call. For now just that, later; possibly updating the // merge methods.
+    # (deSystemize system (cfg.inputs // additionalInputs))
     // {
       inherit self;
-      cells = deSystemize system cells; # recursion on cells
+
+      cells = cells; # recursion on cells
+      #FIXME: Dropped the below deSystemize call; and let cells be cells
+      # cells = deSystemize system cells; # recursion on cells
     }
     // l.optionalAttrs (currentNixpkgs != null) {
       nixpkgs =
